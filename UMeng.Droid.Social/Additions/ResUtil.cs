63535754500eb64086221d88072c4cd2a7a99477
y-z-f <976691141@qq.com>
2016-04-27 21:29:02 +0800

    fix bind bug

--- UMeng.Droid.Social/Additions/ResUtil.cs
+++ UMeng.Droid.Social/Additions/ResUtil.cs
@@ -1,25 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-
 namespace Com.Umeng.Socialize.Utils
 {
-    public partial class ResUtil:global::Java.Lang.Object
-    {
-        public partial class FetchTask : Android.OS.AsyncTask
-        {
-            protected override Java.Lang.Object DoInBackground(params Java.Lang.Object[] @params)
-            {
-                return DoInBackgrounds(@params);
-            }
-         }
-    }
+	public partial class ResUtil : global::Java.Lang.Object
+	{
+		public partial class FetchTask : Android.OS.AsyncTask
+		{
+			protected override Java.Lang.Object DoInBackground(params Java.Lang.Object[] @params)
+			{
+				return DoInBackgrounds(@params);
+			}
+		}
+	}
 }
